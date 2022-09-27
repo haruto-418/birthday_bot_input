2062ce35cd8a2da69fab834f746b09408bbf88e2
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-27 17:19:07 +0900

    add: type.ts, fix: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,57 @@
-import { Stack } from '@chakra-ui/react';
-import { NextPage } from 'next';
 import React from 'react';
+import { NextPage } from 'next';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import {
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { User } from '../lib/type';
 
 const Home: NextPage = () => {
-  return <Stack></Stack>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>();
+
+  const doSubmit: SubmitHandler<any> = async (data: User) => {};
+
+  return (
+    <Stack w={'70%'} mx={'auto'} py={20} spacing={4}>
+      <Heading>Create User</Heading>
+      {/* User Name */}
+      <FormControl>
+        <FormLabel>user name</FormLabel>
+        <Input {...register('name', { required: '名前が未記入です。' })} />
+        <Text color={'red'}>{errors.name?.message}</Text>
+      </FormControl>
+      {/* Birthday */}
+      <FormControl>
+        <FormLabel>Birthday</FormLabel>
+        <Input
+          {...register('birthday', { required: '誕生日が未記入です。' })}
+        />
+        <Text color={'red'}>{errors.birthday?.message}</Text>
+      </FormControl>
+      <Center>
+        <Button
+          w={200}
+          p={6}
+          bg={'teal.100'}
+          fontWeight={'bold'}
+          onClick={() => handleSubmit(doSubmit)()}
+        >
+          submit
+        </Button>
+      </Center>
+    </Stack>
+  );
 };
 
 export default Home;
